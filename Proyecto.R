4e6e2c15977a8a2033a835127631bc39573e1580
unknown <maalvaran@poligran.edu.co>
2021-12-19 19:38:29 -0500

    Informe_Terminado

--- Proyecto.R
+++ Proyecto.R
@@ -2,175 +2,107 @@
 
 library(tidyverse)
 library(ggplot2)
+library(DT)
+library(readxl)
 
 
 #Carga de datos
+Tabla_Final= read_xlsx("Data/Tabla_Final.xlsx")
+
 dim(Tabla_Final)
 
+#Modificar nombre de atributos:
+names(Tabla_Final)[1]="Identificador"
+names(Tabla_Final)[2]= "Tipo_Titulo"
+names(Tabla_Final)[3]= "Nombre_Pelicula"
+names(Tabla_Final)[4]= "Adulto"
+names(Tabla_Final)[5]= "Año_Inicio"
+names(Tabla_Final)[6]= "Duración_Pelicula"
+names(Tabla_Final)[7]= "Genero"
+names(Tabla_Final)[8]= "Rating"
+names(Tabla_Final)[9]= "Numero_Votos"
+
+
+#Validación datos nulos:
+
+Nulos= 2.865
+Registros_totales= 171.743
+
+Porcentaje_nulos = (Nulos/Registros_totales*100)
+Porcentaje_nulos
+
+
 #Grafico de barras Año/número de votos
-ggplot(data=Tabla_Final, aes(x=startYear,y=numVotes)) + geom_bar(stat="identity", position="stack")
-
-#Necesito saber el promedio de votantes por año:
-
-Votante_año = Tabla_Final %>% group_by(startYear) %>% summarize(promedio_votantes = mean(numVotes))
-
-#grafico de barras promedio de votantes año:
-
-ggplot(data=Votante_año, aes(x=startYear,y=promedio_votantes)) + geom_bar(stat="identity", position="stack")
-
-#Necesito saber el número de titulos sacados por año:
-
-Peliculas_año = data.frame(table(Tabla_Final$startYear))
-
-#Grafico de titulos sacados por año:
-
-plot(x = Peliculas_año)
+ggplot(data=Tabla_Final, aes(x=Año_Inicio,y=Numero_Votos)) + geom_bar(stat="identity")
 
 
 
-#Filtro Tabla_Final del año 2009 al año 2019 (por la pandemia)
+# Filtrar Base de datos eliminando el año 2020 y 2021
 
-Data_Final= Tabla_Final %>%  filter(startYear>2008&startYear<2020)
+Data_Final= Tabla_Final %>%  filter(Año_Inicio>=2000&Año_Inicio<2019)
+
+
+# Selección de variables importantes según hipotesis:
+
+Data_nueva= Data_Final %>% select(Genero,Rating,Numero_Votos,Año_Inicio)
+
+
+# En total tengo un total de 999 generos de pelicula:
+unique(Data_nueva$Genero)
+
 
 # Generar top 5 generos con más votos
 
-Top5_generos = Data_Final %>% group_by(genres) %>% summarize(promedio_votantes = mean(numVotes)) %>%
-  arrange(desc(promedio_votantes))
+Top5_generos = Data_nueva %>% group_by(Genero) %>% summarize(Promedio_votantes = sum(Numero_Votos)) %>%
+arrange(desc(Promedio_votantes))
 
-generos_seleccionados= c("Adventure,Drama,War","Action,Adventure,Sci-Fi","Adventure,Fantasy,Mystery","Adventure,Mystery,Sci-Fi","	
-Adventure,Drama,Sci-Fi")
+generos_seleccionados= c("Action,Adventure,Sci-Fi","Adventure,Animation,Comedy","Comedy,Drama,Romance","Action,Crime,Drama","Drama")
 
-Filtro_Top5= Data_Final%>% filter(genres %in% generos_seleccionados)
+Data_Top5_generos= Data_nueva%>% filter(Genero %in% generos_seleccionados)
 
-#grafico de genero más votados:
+# Hipotesis correcta:
+Votos_Genero_ = Data_Top5_generos%>% group_by(Genero) %>% 
+  summarize(Suma_votacion= sum(Numero_Votos)) %>% 
+  mutate(Promedio_votacion= round(Suma_votacion/119481869*100,2))%>% 
+  arrange(desc(Suma_votacion))
 
-ggplot(data=Filtro_Top5, aes(x=genres,y=numVotes)) + geom_bar(stat="identity", position="stack")
+sum(Votos_Genero_$Suma_votacion)
+# GRAFICOS
+ggplot(Votos_Genero_,mapping = aes(x="",y=Promedio_votacion, fill=Genero))+
+  geom_bar(stat = "identity",color="white")+
+  geom_text(aes(label=Promedio_votacion),
+            position=position_stack(vjust=0.5))
 
-#Número de titulos por cada genero:
+#Barras con número de votos:
 
-Titulos_Genero = data.frame(table(Filtro_Top5$genres))
+ggplot(data=Votos_Genero_, aes(x=Genero,y=Suma_votacion)) + geom_bar(stat="identity", position="stack")+
+  theme(axis.text.x=element_text(angle=90, hjust=1))
 
-#grafico de titulos por cada genero:
 
-ggplot(Titulos_Genero,aes(x="",y=Freq, fill=Var1))+
-  geom_bar(stat = "identity",
-           color="white")+
-  geom_text(aes(label=Freq),
-            position=position_stack(vjust=0.5),color="white",size=6)+
-  coord_polar(theta = "y")
+# se filtra por los generos que quedaron en 2,3,4,5 lugar:
+generos_seleccionado= c("Adventure,Animation,Comedy","Comedy,Drama,Romance","Action,Crime,Drama","Drama")
 
-#Pelicula con mejor Rating:
+Data_Top5_gener= Data_nueva%>% filter(Genero %in% generos_seleccionado)
 
-Top5_Rating = Data_Final %>% group_by(genres) %>% summarize(promedio_Rating = mean(averageRating)) %>%
-  arrange(desc(promedio_Rating))
+# Promedio de los generos que quedaron en 2,3,4,5, lugar
+mean(Votos_Genero_$Suma_votacion)
 
-Rating_seleccionado= c("Drama,History,News","Documentary,News,Reality-TV","Action,History,Musical","Action,Fantasy,Musical","Comedy,Talk-Show")
 
-Resumen_Rating= Data_Final %>% filter(genres %in% Rating_seleccionado)
 
-#grafico de Rating:
-  
-ggplot(data=Resumen_Rating, aes(x=Rating_seleccionado,y=averageRating)) + geom_bar(stat="identity", position="stack")
+         
 
-# graficnúmero de votos por genero mejor Rating.
 
-ggplot(data=Resumen_Rating, aes(x=Rating_seleccionado,y=numVotes)) + geom_bar(stat="identity", position="stack")
 
-# ahora se seleccionara las peliculas con mejor Rating:
 
-Data_ordenada_Votos= Data_Final %>% arrange(desc(numVotes))
 
 
-# peliculas top 5: Falta hacer grafico() voy acá
 
-Top5_Peliculas= c("Inception","Interstellar","The Dark Knight Rises","Django Unchained","Inglourious Basterds")
 
-Resumen_peliculas_Top= Data_Final %>% filter(numVotes %in% Top5_Peliculas)
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Filtrar por top 5 más votados:
-
-Top5_generos_peliculas=c("Action,Adventure,Sci-Fi","Adventure,Drama,Sci-Fi","Action,Crime,Drama","Drama,Western","Adventure,Drama,War")
-
-Resumen_Top_genero_peli= Data_Final %>% filter(genres %in% Top5_generos_peliculas)
-
-Peliculas_año_generosTop5 = data.frame(table(Resumen_Top_genero_peli$numVotes))
-
-Peliculas_año_ = data.frame(table(Peliculas_año_generosTop5$numVotes))
-
-
-# Generos que más votación tienen
-contar_genero= Tabla_Final%>% count(genres)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-str(Tabla_Final %>% group_by(startYear))
-
-
-
-count(Tabla_Final,"startYear")
-
-
-
-str(Tabla_Final$numVotes)
-
-#Base de datos resumida para cada una de las variables:
-  
-summary(Tabla_Final)
-Tabla_Final$averageRating
-
-
-#Base de datos filtrada por genero y número de votos:
-
-Genero_Votos= Tabla_Final %>% group_by(genres) %>% summarise(suma_votos= sum(numVotes)) %>%
-              arrange(desc(suma_votos))
-
-
-#Base de datos filtrada por número de votos:
-
-Genero_Rating= Tabla_Final %>% group_by(genres) %>% summarise(Rating_promedio= mean(averageRating)) %>%
-  arrange(desc(Rating_promedio))
-
-
-
-#Base de datos filtrada por mejores generos:
-
-generos_seleccionados= c("Action,Crime,Drama","Action,Adventure,Sci-Fi","Action,Adventure,Drama")
-
-#Base de datos filtrada porlos 3 generos más votados:
-
-generos_mas_votados= Tabla_Final %>% filter(genres %in% generos_seleccionados)
 
 
 
